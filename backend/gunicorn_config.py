943e4b00f8208747364420b90d19fe9902fcc64c
felipeOliveira-1 <contato@felipesilva.tec>
2024-12-13 09:07:31 -0300

    update

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,8 +1,34 @@
+import os
 import multiprocessing
 
-bind = "0.0.0.0:10000"
+# Server socket
+bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
+backlog = 2048
+
+# Worker processes
 workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
 timeout = 120
-accesslog = "-"
-errorlog = "-"
-capture_output = True
+keepalive = 2
+
+# Logging
+errorlog = '-'
+loglevel = 'info'
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = 'portfolio-crypto-backend'
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL
+keyfile = None
+certfile = None
